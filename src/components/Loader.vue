6d214e08c344509d21bf541f90333cbc4a2b64ca
yattdev <yattdeveloper@gmail.com>
2021-11-10 10:36:26 +0100

    Create Spinner/loading component

---
+++ src/components/Loader.vue
@@ -0,0 +1,36 @@
+<template>
+    <Spinner class="loader" :name="loading ? 'circle' : ''" color="#32B5B6"/>
+</template>
+
+<script>
+import Vue from 'vue'
+import Spinner from 'vue-spinkit'
+
+export default Vue.component('Loader',{
+    props: [ 'isLoading' ],
+    data() {
+        return { }
+    },
+    computed: {
+        loading(){
+        console.log(this.isLoading);
+            return this.isLoading;
+        }
+    },
+    components: {
+        "Spinner": Spinner,
+    }
+})
+</script>
+
+
+<style scoped>
+.loader {
+    left: 50%;
+    margin-left: -4em;
+    top: 50%;
+    margin-top: 4em;
+    width: 100px;
+    height: 100px;
+}
+</style>
